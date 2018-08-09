c56bb556d46f6c7616e7ff48f8a494d5a44d0e5f
Hillevi Eklöw <hillevieklow@gmail.com>
2018-08-09 18:30:45 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if !params[:name].present?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+
   end
 
   def destroy
